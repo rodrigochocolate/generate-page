40fa939cc95b2a8b7378007232b9ce740534ec51
Rodrigo Chocolate <rodrigodev2021@gmail.com>
2021-10-02 19:36:29 -0300

    fixing link:css

--- generate-page.py
+++ generate-page.py
@@ -4,8 +4,8 @@ template_HTML = '''<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="reset.css">
-  <link rel="stylesheet" href="template.css">
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/template.css">
   <title>Template</title>
 </head>
 <body>
